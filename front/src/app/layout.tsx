6fb1a8651e63bba25404a576065d16362ec6f53d
SamuelCastrillon <samcastj@gmail.com>
2024-08-19 21:24:08 -0500

    clean default folders and move main tag

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header></header>
+        <main>{children}</main>
+        <footer></footer>
+      </body>
     </html>
   );
 }
