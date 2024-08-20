96e8f8ff9b2a8b9030b3c3323e732c06e7d764bb
MPCarranza <mariapilarcarranza@gmail.com>
2024-08-20 16:20:47 -0300

    add Home Component

---
+++ front/src/views/HomeView/HomeReception.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+const HomeReception: React.FC = () => {
+  return (
+    <>
+    <section className="flex flex-col items-center justify-center w-screen h-screen gap-2">
+        <h1>TORNEOS</h1>
+        <h1>EN UN SOLO</h1>
+        <h1>LUGAR</h1>
+        <p>Todos tus torneos organizados de la mejor manera.<br/>
+        Padel simplificado como nunca viste.</p>
+    </section>
+    </>
+  );
+};
+
+export default HomeReception;
