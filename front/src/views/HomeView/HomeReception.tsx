d334ddd4057f41d5f96f121d646feecbc268730a
MPCarranza <mariapilarcarranza@gmail.com>
2024-08-20 16:48:49 -0300

    still working on HomeComponent

--- front/src/views/HomeView/HomeReception.tsx
+++ front/src/views/HomeView/HomeReception.tsx
@@ -5,13 +5,13 @@ import React from "react";
 const HomeReception: React.FC = () => {
   return (
     <>
-    <section className="flex flex-col items-center justify-center w-screen h-screen gap-2">
-        <h1>TORNEOS</h1>
-        <h1>EN UN SOLO</h1>
-        <h1>LUGAR</h1>
+        <div className="flex flex flex-col justify-start items-start">
+        <h1>TORNEOS</h1><br/>
+        <h1>EN UN SOLO</h1><br/>
+        <h1>LUGAR</h1><br/>
         <p>Todos tus torneos organizados de la mejor manera.<br/>
         Padel simplificado como nunca viste.</p>
-    </section>
+        </div>
     </>
   );
 };
