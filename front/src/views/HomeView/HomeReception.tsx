8d0c2bb605b7c66bb759e3e9acb1d830fcda1103
MPCarranza <mariapilarcarranza@gmail.com>
2024-08-20 17:34:55 -0300

    still working

--- front/src/views/HomeView/HomeReception.tsx
+++ front/src/views/HomeView/HomeReception.tsx
@@ -5,10 +5,12 @@ import React from "react";
 const HomeReception: React.FC = () => {
   return (
     <>
-        <div className="flex flex flex-col justify-start items-start">
+        <div className="m-4 flex flex flex-col justify-start items-start">
+        <div className="text-lime-800 font-bold">
         <h1>TORNEOS</h1><br/>
         <h1>EN UN SOLO</h1><br/>
         <h1>LUGAR</h1><br/>
+        </div>
         <p>Todos tus torneos organizados de la mejor manera.<br/>
         Padel simplificado como nunca viste.</p>
         </div>
