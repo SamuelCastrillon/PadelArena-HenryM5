f7c3cc07bbdecf23fc03140d5b518a02d56b6b7b
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-08-20 10:15:23 -0300

    tailwind config colors

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        lime: "#BEF164",
+        customBlue: "#1D4ED7",
+      },
     },
   },
   plugins: [],
