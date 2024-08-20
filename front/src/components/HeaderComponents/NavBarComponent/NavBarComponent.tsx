ce237a2c05772b82dbff08882950acd690f50104
SamuelCastrillon <samcastj@gmail.com>
2024-08-20 13:03:18 -0500

    add menu dropDown to movile view

--- front/src/components/HeaderComponents/NavBarComponent/NavBarComponent.tsx
+++ front/src/components/HeaderComponents/NavBarComponent/NavBarComponent.tsx
@@ -1,37 +1,51 @@
+"use client";
+import ActionButton from "@/components/GeneralComponents/ActionButton/ActionButton";
 import { NavigateButton } from "@/components/GeneralComponents/NavigateButton/NavigateButton";
 
 import React from "react";
 
 const NavBarComponent: React.FC = () => {
+  const [dropDawn, setDropDown] = React.useState(false);
+  function handleDropDown() {
+    setDropDown(!dropDawn);
+  }
   return (
-    <nav className="flex gap-8 mt-8 h-[64px] w-[90%] bg-[#808080b7] justify-center bg-opacity-80 items-center rounded-2xl font-bold ">
-      <img src="logoApp.png" alt="Logo" className="w-auto h-[80%]" />
-      <NavigateButton
-        href="/"
-        className="px-5 py-2 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
-        INICIO
-      </NavigateButton>
-      <NavigateButton
-        href="#"
-        className="px-5 py-2 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
-        TORNEOS
-      </NavigateButton>
-      <NavigateButton
-        href="#"
-        className="px-5 py-2 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
-        NOTICIAS
-      </NavigateButton>
-      <div className="flex items-center h-full gap-4">
+    <nav className="flex flex-col gap-2 md:flex-row md:gap-4 mt-8 h-fit py-5 sm:py-0 sm:h-[64px] w-screen md:w-[95%] bg-[#808080b7] justify-center bg-opacity-80 items-center rounded-2xl font-bold ">
+      <img src="logoApp.png" alt="Logo" className="w-auto h-[60px] sm:h-[85%]" />
+      <ActionButton className="block sm:hidden" onClick={handleDropDown}>
+        <div className="h-[30px] w-[30px] bg-slate-200 rounded-[50%]"></div>
+      </ActionButton>
+      <div
+        className={`flex-col items-center ${
+          dropDawn ? "flex" : "hidden"
+        } gap-4 sm:flex-row sm:flex`}>
         <NavigateButton
-          href="/login"
-          className="px-5 py-2 text-black rounded-lg h-fit w-fit bg-lime-300 hover:mb-[2px] hover:shadow-lg">
-          INGRESAR
+          href="/"
+          className="px-5 py-2 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
+          INICIO
         </NavigateButton>
         <NavigateButton
-          href="/register"
-          className="px-5 py-2 bg-blue-700 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
-          CREAR CUENTA
+          href="#"
+          className="px-5 py-2 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
+          TORNEOS
         </NavigateButton>
+        <NavigateButton
+          href="#"
+          className="px-5 py-2 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
+          NOTICIAS
+        </NavigateButton>
+        <div className="flex flex-col items-center h-full gap-4 sm:flex-row">
+          <NavigateButton
+            href="/login"
+            className="px-5 py-2 text-black rounded-lg h-fit w-fit bg-lime-300 hover:mb-[2px] hover:shadow-lg">
+            INGRESAR
+          </NavigateButton>
+          <NavigateButton
+            href="/register"
+            className="px-5 py-2 bg-blue-700 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
+            CREAR CUENTA
+          </NavigateButton>
+        </div>
       </div>
     </nav>
   );
