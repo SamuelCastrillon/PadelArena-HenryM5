225d10bc122650b7f009364b552de7241a6b3dd9
MPCarranza <mariapilarcarranza@gmail.com>
2024-08-20 18:18:47 -0300

    still working

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,10 +3,6 @@ import HomeReception from "@/views/HomeView/HomeReception"
 export default function Home() {
   return (
     <>
-    <section 
-    className="flex items-center justify-center w-screen h-screen gap-2">
-      <h1 className="text-3xl font-bold">Modulo 5</h1>
-    </section>
     <HomeReception/>
     </>
   );
--- front/src/views/HomeView/HomeReception.tsx
+++ front/src/views/HomeView/HomeReception.tsx
@@ -5,15 +5,20 @@ import React from "react";
 const HomeReception: React.FC = () => {
   return (
     <>
-        <div className="m-4 flex flex flex-col justify-start items-start">
-        <div className="text-lime-800 font-bold">
-        <h1>TORNEOS</h1><br/>
-        <h1>EN UN SOLO</h1><br/>
-        <h1>LUGAR</h1><br/>
+    <div className="global-container flex justify-start items-center min-h-screen w-full">
+        <div className="text-container m-[30px] p-4 flex flex-col justify-start items-start">
+          <div className="tittles text-[#BEF164] font-bold font-radhiumz">
+          <h1 className="text-[30px]">TORNEOS</h1>
+          <h1 className="text-[30px]">EN UN SOLO</h1>
+          <h1 className="text-[30px]">LUGAR</h1>
+          </div>
+          <p>Todos tus torneos organizados de la mejor manera.<br />
+          Padel simplificado como nunca viste.</p>
+          <button className="mt-4 rounded-md bg-[#BEF164] min-w-[150px] px-4 py-2 text-white">
+          ver torneos disponibles
+          </button>
         </div>
-        <p>Todos tus torneos organizados de la mejor manera.<br/>
-        Padel simplificado como nunca viste.</p>
-        </div>
+    </div>
     </>
   );
 };
