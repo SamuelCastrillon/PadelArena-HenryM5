8b65125f8336ffbb6fddbe3d448c90c715ced580
SamuelCastrillon <samcastj@gmail.com>
2024-08-20 18:50:39 -0500

    Modify required inputs

--- front/src/mainPages/CreateTournamentView/CreateTournamentData.ts
+++ front/src/mainPages/CreateTournamentView/CreateTournamentData.ts
@@ -5,33 +5,58 @@ import {
 } from "@/components/MainComponents/ReusableFormComponent/FormInterface";
 
 export const createTournamentInitialValues = {
-  email: "",
-  password: "",
+  name: "",
+  date: "",
+  startTime: "",
+  endTime: "",
+  categorie: "",
+  description: "",
 };
 
 //? Validations Inpusts form
 export const createTournamentSchema = yup.object({
-  email: yup
-    .string()
-    .matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, "Invalid email")
-    .defined("Required!"),
+  name: yup.string().min(2, "Too Short!").max(40, "Too Long!").defined("Required!"),
   password: yup.string().min(8, "Too Short!").max(40, "Too Long!").defined("Required!"),
 });
 
 //? Data constructor form
 export const inputsCreateTournamentFormValues: IDataConstructorInput[] = [
   {
-    LabelText: "User Email",
-    FieldType: "email",
-    FieldName: "email",
-    FieldPH: "example@mail.com",
+    LabelText: "Nombre Del Torneo",
+    FieldType: "text",
+    FieldName: "name",
+    FieldPH: "Padel Arena",
   },
   {
-    LabelText: "Password",
-    FieldType: "password",
-    FieldName: "password",
-    FieldPH: "********",
+    LabelText: "Fecha de Inicio",
+    FieldType: "date",
+    FieldName: "date",
+    FieldPH: "01/01/2025",
+  },
+  {
+    LabelText: "Hora de Inicio",
+    FieldType: "time",
+    FieldName: "startTime",
+    FieldPH: "00:00",
+  },
+  {
+    LabelText: "Hora de Conclusion",
+    FieldType: "time",
+    FieldName: "endTime",
+    FieldPH: "00:00",
+  },
+  {
+    LabelText: "Categoria del Torneo",
+    FieldType: "text",
+    FieldName: "categorie",
+    FieldPH: "Categoria 01",
+  },
+  {
+    LabelText: "Descripción",
+    FieldType: "text",
+    FieldName: "description",
+    FieldPH: "Descripción del Torneo",
   },
 ];
 
-export const butonsCreateTournamentForm: IButtonForm[] = [{ name: "Sign In", type: "submit" }];
+export const butonsCreateTournamentForm: IButtonForm[] = [{ name: "Crear Torneo", type: "submit" }];
