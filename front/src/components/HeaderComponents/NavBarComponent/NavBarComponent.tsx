eb2c5494ebbfc4406f9cf41435c364be73eceb3a
SamuelCastrillon <samcastj@gmail.com>
2024-08-20 23:35:07 -0500

    fix navBar styles

--- front/src/components/HeaderComponents/NavBarComponent/NavBarComponent.tsx
+++ front/src/components/HeaderComponents/NavBarComponent/NavBarComponent.tsx
@@ -11,7 +11,7 @@ const NavBarComponent: React.FC = () => {
     setDropDown(!dropDawn);
   }
   return (
-    <nav className="flex flex-col gap-2 md:flex-row md:gap-4 mt-8 h-fit py-5 sm:py-0 sm:h-[64px] w-screen md:w-[95%] bg-[#808080b7] justify-center bg-opacity-80 items-center rounded-2xl font-bold ">
+    <nav className="flex flex-col gap-2 md:flex-row md:gap-4 mt-8 h-fit py-5 sm:py-0 sm:h-[64px] w-screen md:w-[95%] bg-[#808080b7] justify-center bg-opacity-80 items-center rounded-2xl font-bold text-white">
       <img src="logoApp.png" alt="Logo" className="w-auto h-[60px] sm:h-[85%]" />
       <ActionButton className="block sm:hidden" onClick={handleDropDown}>
         <Bars3Icon className="h-10 font-bold" />
@@ -39,12 +39,12 @@ const NavBarComponent: React.FC = () => {
         <div className="flex flex-col items-center h-full gap-4 sm:flex-row">
           <NavigateButton
             href="/login"
-            className="px-5 py-2 text-black rounded-lg h-fit w-fit bg-lime-300 hover:mb-[2px] hover:shadow-lg">
+            className="px-5 py-2 rounded-lg text-black bg-lime h-fit w-fit bg-lime-300 hover:mb-[2px] hover:shadow-lg">
             INGRESAR
           </NavigateButton>
           <NavigateButton
             href="/register"
-            className="px-5 py-2 bg-blue-700 rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
+            className="px-5 py-2 bg-blue-700  rounded-lg h-fit w-fit hover:mb-[2px] hover:shadow-lg">
             CREAR CUENTA
           </NavigateButton>
         </div>
