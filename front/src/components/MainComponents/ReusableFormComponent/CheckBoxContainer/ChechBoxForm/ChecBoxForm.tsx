dd0ee3b53d2d07e33b256f6eb8d562263589228b
SamuelCastrillon <samcastj@gmail.com>
2024-08-21 10:07:21 -0500

    fix formComponent Responsive styles

--- front/src/components/MainComponents/ReusableFormComponent/CheckBoxContainer/ChechBoxForm/ChecBoxForm.tsx
+++ front/src/components/MainComponents/ReusableFormComponent/CheckBoxContainer/ChechBoxForm/ChecBoxForm.tsx
@@ -4,7 +4,7 @@ import { Field } from "formik";
 
 const ChecBoxForm: React.FC<IDataConstructor> = ({ LabelText, FieldType, FieldName }) => {
   return (
-    <div className="flex flex-col gap-1 sm:flex-row w-fit h-fit">
+    <div className="flex gap-1 w-fit h-fit">
       <label className="font-bold text-customBlue">{LabelText}</label>
       <Field name={FieldName} type={FieldType}></Field>
     </div>
