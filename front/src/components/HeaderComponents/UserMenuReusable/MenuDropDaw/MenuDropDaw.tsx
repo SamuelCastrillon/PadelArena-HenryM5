88c848a0209fa31e3711d19df18d5397e2c315a1
MPCarranza <mariapilarcarranza@gmail.com>
2024-08-22 18:24:35 -0300

    changes on tournamentsTable + changes on bg-NavBar and bg-dropdownMenu

--- front/src/components/HeaderComponents/UserMenuReusable/MenuDropDaw/MenuDropDaw.tsx
+++ front/src/components/HeaderComponents/UserMenuReusable/MenuDropDaw/MenuDropDaw.tsx
@@ -11,12 +11,17 @@ import { IMenuReusableStatus } from "../UserMenuReusableInterfaces";
 
 const MenuDropDaw: React.FC<IMenuReusableStatus> = ({ menuStatus }) => {
   return (
-    <div className={`absolute top-20 right-0 ${menuStatus ? "block" : "hidden"}`}>
-      <menu className="flex flex-col   w-[360px] h-auto bg-glass backdrop-blur-glass backdrop-filter-glass bg-opacity-80 rounded-2xl p-8 sfRegular border-glass border-2">
-        <MenuButton text="Crear Torneo" routeNavigate="dashboard/admin/tournaments/create">
+    <div
+      className={`absolute top-20 right-0 ${menuStatus ? "block" : "hidden"}`}>
+      <menu className="flex flex-col   w-[360px] h-auto bg-glass backdrop-filter bg-white/30 border-glass border-2 shadow-glass rounded-2xl p-8 sfRegular">
+        <MenuButton
+          text="Crear Torneo"
+          routeNavigate="dashboard/admin/tournaments/create">
           <PlusCircleIcon className="h-10" />
         </MenuButton>
-        <MenuButton text="Gestion de Torneos">
+        <MenuButton
+          text="Gestion de Torneos"
+          routeNavigate="dashboard/admin/tournaments/management">
           <Cog6ToothIcon className="h-10 " />
         </MenuButton>
         <hr className="w-full h-[1px] mt-2 mb-2 border-none bg-white" />
