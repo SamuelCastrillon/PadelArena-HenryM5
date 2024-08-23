8469ca9523239d356b7eb7c6614063e2d07879ae
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-08-23 12:11:06 -0300

    commit sin back

--- back/src/fixture/fixture.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FixtureService } from './fixture.service';
-import { FixtureController } from './fixture.controller';
-
-@Module({
-  controllers: [FixtureController],
-  providers: [FixtureService],
-})
-export class FixtureModule {}
--- back/src/player-stadistics/player-stadistics.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlayerStadisticsService } from './player-stadistics.service';
-import { PlayerStadisticsController } from './player-stadistics.controller';
-
-@Module({
-  controllers: [PlayerStadisticsController],
-  providers: [PlayerStadisticsService],
-})
-export class PlayerStadisticsModule {}
