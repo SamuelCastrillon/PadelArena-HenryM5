5924ded6661eae57368e60db778a793bcde55a4e
SamuelCastrillon <samcastj@gmail.com>
2024-08-23 19:30:30 -0500

    add Select Field type component to Forms Reusable component an modify Create tournament form

--- front/src/app/dashboard/admin/tournaments/create/page.tsx
+++ front/src/app/dashboard/admin/tournaments/create/page.tsx
@@ -1,8 +1,9 @@
+import { getDataToContructFormCreateTournaments } from "@/components/MainPages/CreateTournamentView/CreateTournamentData";
 import CreateTournamentView from "@/components/MainPages/CreateTournamentView/CreateTournamentView";
 import React from "react";
 
-const page: React.FC = () => {
-  return <CreateTournamentView />;
+const page: React.FC = async () => {
+  return <CreateTournamentView dataContructor={await getDataToContructFormCreateTournaments()} />;
 };
 
 export default page;
--- front/src/components/MainPages/CreateTournamentView/CreateTournamentView.tsx
+++ front/src/components/MainPages/CreateTournamentView/CreateTournamentView.tsx
@@ -5,28 +5,32 @@ import {
   butonsCreateTournamentForm,
   createTournamentInitialValues,
   createTournamentSchema,
-  inputsCreateTournamentFormValues,
+  getDataToContructFormCreateTournaments,
 } from "./CreateTournamentData";
 import { preFormattingData } from "./PreFormattingData";
 import { ICreateTournamentFormData } from "@/interfaces/RequestInterfaces";
 import HandlerNewTournament from "@/Server/HandlerFormsFuctions/HandlerCreateTournament";
+import { IDataConstructor } from "@/components/MainComponents/ReusableFormComponent/FormInterface";
 
 async function handlerSubmit(values: ICreateTournamentFormData) {
+  console.log(values);
+
   const dataFormattedToSend = preFormattingData(values);
   if (dataFormattedToSend) {
-    const response = await HandlerNewTournament(dataFormattedToSend);
-    console.log(response);
+    // const response = await HandlerNewTournament(dataFormattedToSend);
+    // console.log(response);
+    // console.log(dataFormattedToSend);
   }
 }
 
-const CreateTournamentView: React.FC = () => {
+const CreateTournamentView = ({ dataContructor }: { dataContructor: IDataConstructor[] }) => {
   return (
     <section className="flex flex-col items-center justify-center w-screen gap-2 h-fit">
       <FormComponent
         iniValues={createTournamentInitialValues}
         valiSchema={createTournamentSchema}
         handelerSubmit={handlerSubmit}
-        dataContructor={inputsCreateTournamentFormValues}
+        dataContructor={dataContructor}
         butonsForm={butonsCreateTournamentForm}
       />
     </section>
