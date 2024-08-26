84473f44d7f6347e776e3f32b23b8690aafa6190
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-08-26 11:49:49 -0300

    commit all working

--- src/Server/Category/getCategories.ts
+++ src/Server/Category/getCategories.ts
@@ -2,6 +2,7 @@ import { axiosInstance } from "../AxiosConfig";
 export const getCategories = async () => {
   try {
     const response = await axiosInstance.get("/category");
+    console.log(response);
     return response.data;
   } catch (error) {
     console.log(error);
