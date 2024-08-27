8e58f2cb8c720d0762ab8be64671f5f38fb57979
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-08-26 21:28:50 -0300

    update user panel

--- src/components/MainPages/DashboardUser/UserInfoPanel/UserInfoPanel.tsx
+++ src/components/MainPages/DashboardUser/UserInfoPanel/UserInfoPanel.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { IUserLogin } from "@/interfaces/RequestInterfaces";
 import Image from "next/image";
+import ActionButton from "@/components/GeneralComponents/ActionButton/ActionButton";
+
 const UserInfoPanel: React.FC<{ user: IUserLogin }> = ({ user }) => {
+  const [userInfo, setUserInfo] = useState<IUserLogin>(user);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const toggleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
   const getImageUrl = (src: string) => {
     const defaultImage = "/images/default-image.jpg";
     const isValidUrl =
@@ -10,37 +25,117 @@ const UserInfoPanel: React.FC<{ user: IUserLogin }> = ({ user }) => {
       src.startsWith("/");
     return isValidUrl ? src : defaultImage;
   };
+
   return (
     <div className="bg-gray-100 p-6 rounded-lg text-center">
       <Image
-        src={getImageUrl(user.profileImg || "/images/default-image.jpg")}
+        src={getImageUrl(userInfo.profileImg || "/images/default-image.jpg")}
         alt="Profile Picture"
         className="rounded-full mx-auto mb-4"
         width={100}
         height={100}
       />
-      <h2 className="text-xl font-semibold mb-2">
-        {user.name} {user.lastName}
-      </h2>
-      <p className="text-gray-600 mb-4">{user.role}</p>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            name="name"
+            value={userInfo.name}
+            onChange={handleChange}
+            className="text-xl sfRegular mb-2 p-1 border-b border-gray-300 rounded w-full"
+          />
+          <input
+            type="text"
+            name="lastName"
+            value={userInfo.lastName}
+            onChange={handleChange}
+            className="text-xl sfRegular mb-2 p-1 border-b border-gray-300 rounded w-full"
+          />
+        </>
+      ) : (
+        <h2 className="text-xl radhiumz mb-2">
+          {userInfo.name} {userInfo.lastName}
+        </h2>
+      )}
+      <p className="uppercase mb-4 sfBold text-green-600">{userInfo.role}</p>
       <ul className="text-left space-y-2">
-        <li>
-          <strong>{user.email}</strong>
+        <li className="border-b border-gray-300 pb-2">
+          {isEditing ? (
+            <input
+              type="email"
+              name="email"
+              value={userInfo.email}
+              onChange={handleChange}
+              className="p-1 border-b border-gray-300 rounded w-full"
+            />
+          ) : (
+            <p className="sfRegular">
+              Tu Email: <span className="sfBold">{userInfo.email}</span>
+            </p>
+          )}
         </li>
-        <li>
-          <strong>{user.phone}</strong>
+        <li className="border-b border-gray-300 pb-2">
+          {isEditing ? (
+            <input
+              type="text"
+              name="phone"
+              value={userInfo.phone}
+              onChange={handleChange}
+              className="p-1 border-b border-gray-300 rounded w-full"
+            />
+          ) : (
+            <p className="sfRegular">
+              Tu Telefono: <span className="sfBold">{userInfo.phone}</span>
+            </p>
+          )}
         </li>
-        <li>
-          <strong>
-            {user.country}, {user.city}
-          </strong>
+        <li className="border-b border-gray-300 pb-2">
+          {isEditing ? (
+            <input
+              type="text"
+              name="address"
+              value={userInfo.address}
+              onChange={handleChange}
+              className="p-1 border-b border-gray-300 rounded w-full"
+            />
+          ) : (
+            <p className="sfRegular">
+              Tu Direccion: <span className="sfBold">{userInfo.address}</span>
+            </p>
+          )}
+        </li>
+        <li className="border-b border-gray-300 pb-2">
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                name="country"
+                value={userInfo.country}
+                onChange={handleChange}
+                className="p-1 border-b border-gray-300 rounded w-full"
+              />
+              <input
+                type="text"
+                name="city"
+                value={userInfo.city}
+                onChange={handleChange}
+                className="p-1 border-b border-gray-300 rounded w-full"
+              />
+            </>
+          ) : (
+            <p className="sfRegular">
+              Tu Pais: <span className="sfBold">{userInfo.country}, </span>
+              Tu Ciudad: <span className="sfBold">{userInfo.city}</span>
+            </p>
+          )}
         </li>
       </ul>
-      <div className="mt-4">
-        <p>
-          <strong>Address:</strong> {user.address}
-        </p>
-      </div>
+      <ActionButton
+        onClick={toggleEdit}
+        className="mt-4 bg-lime text-black px-4 py-2 rounded hover:bg-black hover:text-white"
+      >
+        {isEditing ? "Guardar" : "Editar"}
+      </ActionButton>
     </div>
   );
 };
--- src/components/MainPages/DashboardUser/UserPaymentsTable/UserPaymentsTable.tsx
+++ src/components/MainPages/DashboardUser/UserPaymentsTable/UserPaymentsTable.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { AuthContext } from "@/context/GlobalContext";
+import React, { useContext } from "react";
 
 interface IPayment {
   orderId: string;
@@ -10,17 +11,20 @@ interface IPayment {
 const PaymentHistoryPanel: React.FC<{ payments: IPayment[] }> = ({
   payments,
 }) => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="bg-white p-6 rounded-lg">
-      <h3 className="text-lg font-semibold mb-4">Payment Overview</h3>
+      <h3 className="text-lg font-semibold mb-4">
+        Historial de Pagos de:{" "}
+        <span className="uppercase radhiumz">{currentUser?.name}</span>
+      </h3>
       <table className="w-full table-auto">
         <thead>
           <tr>
-            <th className="px-4 py-2 text-left">Order ID</th>
+            <th className="px-4 py-2 text-left">Id de Orden</th>
             <th className="px-4 py-2 text-left">Status</th>
-            <th className="px-4 py-2 text-left">Total Amount</th>
-            <th className="px-4 py-2 text-left">Order Date</th>
-            <th className="px-4 py-2 text-left">Options</th>
+            <th className="px-4 py-2 text-left">Importe Total</th>
+            <th className="px-4 py-2 text-left">Fecha de la Orden</th>
           </tr>
         </thead>
         <tbody>
@@ -38,11 +42,6 @@ const PaymentHistoryPanel: React.FC<{ payments: IPayment[] }> = ({
               </td>
               <td className="px-4 py-2">${payment.amount}</td>
               <td className="px-4 py-2">{payment.date}</td>
-              <td className="px-4 py-2">
-                <button className="text-blue-500 hover:underline">
-                  Options
-                </button>
-              </td>
             </tr>
           ))}
         </tbody>
