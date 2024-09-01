4c958561cdf73943078f6098127e4490a3e29185
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-09-01 18:56:16 -0300

    update tournaments inscription user

--- src/app/dashboard/user/my-tournaments/page.tsx
+++ src/app/dashboard/user/my-tournaments/page.tsx
@@ -1,15 +1,19 @@
 import TournamentSectionAll from "@/components/MainComponents/TournamentAllGallery/TournamenSectionAll";
+import PersonalizedTournamentView from "@/components/MainPages/DashboardUser/PersonalizedTournamentView/PersonalizedTournamentView";
 import StadisticsView from "@/components/MainPages/DashboardUser/StadisticsSection/StadisticsView";
-import PersonalizedTournamentView from "@/components/MainPages/PersonalizedTournamentView/PersonalizedTournamentView";
+import UserTournaments from "@/components/MainPages/DashboardUser/UserTournaments/UserTournaments";
 
 import React from "react";
 
 const Stadistics = () => {
   return (
     <div className="flex flex-col w-full">
-      <StadisticsView />
+      <UserTournaments />
 
-      <PersonalizedTournamentView />
+      <div className="bg-blue-700/30">
+        <PersonalizedTournamentView />
+        <StadisticsView />
+      </div>
     </div>
   );
 };
--- src/components/MainPages/DashboardUser/StadisticsSection/StadisticsView.tsx
+++ src/components/MainPages/DashboardUser/StadisticsSection/StadisticsView.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import MatchStatsChart from "../../../MainComponents/MatchChart/MatchChart";
 import TournamentLineChart from "../../../MainComponents/TournamentChart/TournamentChart";
+
 const wins = 15; // Número de partidos ganados (puedes obtener estos datos desde tu backend)
 const losses = 5;
 const gano = [5, 8, 3, 6, 7, 9, 4, 2, 8, 10, 6, 9]; // Ejemplo de torneos ganados por mes
@@ -10,12 +11,17 @@ const perdio = [3, 4, 2, 5, 6, 3, 7, 4, 6, 5, 2, 4];
 
 const StadisticsView = () => {
   return (
-    <div className="flex flex-wrap w-full my-20">
-      <div className="w-full md:w-1/2 p-2">
-        <MatchStatsChart wins={wins} losses={losses} />
-      </div>
-      <div className="w-full md:w-1/2 p-2">
-        <TournamentLineChart gano={gano} perdio={perdio} />
+    <div className="flex flex-col justify-center w-full my-6">
+      <h1 className="text-3xl text-center text-white radhiumz uppercase mb-8">
+        Una mirada a tu rendimiento
+      </h1>
+      <div className="flex flex-row gap-8 w-full px-4 ">
+        <div className="flex-1">
+          <MatchStatsChart wins={wins} losses={losses} />
+        </div>
+        <div className="flex-1">
+          <TournamentLineChart gano={gano} perdio={perdio} />
+        </div>
       </div>
     </div>
   );
