571cc4ab83eb23f7e5a60f6f64642ecbaa942fdc
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-09-05 10:10:19 -0300

    update fixture table working

--- src/components/MainComponents/FixtureComponent/NewFixtureComponent.tsx
+++ src/components/MainComponents/FixtureComponent/NewFixtureComponent.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
-import { IFixture } from "@/interfaces/ComponentsInterfaces/Fixture"; // Asegúrate de que la ruta sea correcta
+import { IFixture } from "@/interfaces/ComponentsInterfaces/Fixture";
 import { getFixtureById } from "@/Server/Fixture/getFixtureById";
 import { AuthContext } from "@/context/GlobalContext";
 import { selectWinner } from "@/Server/Fixture/selectWinner";
-import { getOneTeam } from "@/Server/Tournament/getOneTeam";
+import { ITeam } from "@/interfaces/ComponentsInterfaces/Team";
 
 interface FixtureProps {
   fixtureId: string;
@@ -12,10 +12,8 @@ interface FixtureProps {
 const NewFixtureComponent: React.FC<FixtureProps> = ({ fixtureId }) => {
   const [fixture, setFixture] = useState<IFixture | null>(null);
   const [dropdownOpen, setDropdownOpen] = useState<string | null>(null);
-  const [selectedWinner, setSelectedWinner] = useState<string | null>(null);
-  const [winnerNames, setWinnerNames] = useState<{ [key: string]: string }>({});
+
   const { currentUser } = useContext(AuthContext);
-  const [newFixture, setNewFixture] = useState<IFixture | null>(null);
 
   useEffect(() => {
     const getFixture = async () => {
@@ -30,61 +28,27 @@ const NewFixtureComponent: React.FC<FixtureProps> = ({ fixtureId }) => {
     getFixture();
   }, [fixtureId]);
 
-  console.log(fixture);
-
-  useEffect(() => {
-    const updateWinnerNames = async () => {
-      if (fixture) {
-        const winnerNamePromises = fixture.round.flatMap((round) =>
-          round.matches.map(async (match) => {
-            if (match.teamWinner) {
-              const teamWinner = await getOneTeam(match.teamWinner);
-              return { [match.id]: teamWinner.name };
-            }
-            return null;
-          })
-        );
-
-        const resolvedWinnerNames = await Promise.all(winnerNamePromises);
-        const winnerNamesMap = Object.assign(
-          {},
-          ...resolvedWinnerNames.filter((item) => item !== null)
-        );
-
-        setWinnerNames(winnerNamesMap);
-      }
-    };
-
-    updateWinnerNames();
-  }, [fixture]); // Se ejecuta cuando `fixture` cambia
-
   const handleDropdownToggle = (matchId: string) => {
     setDropdownOpen(dropdownOpen === matchId ? null : matchId);
   };
 
   const handleSelectWinner = async (matchId: string, teamId: string) => {
     try {
-      console.log(teamId, matchId);
       const response = await selectWinner(matchId, teamId);
       console.log(response);
+
       console.log(response.matches, response.stage);
       if (response.stage !== undefined && response.matches !== undefined) {
-        // if (fixture && fixture.id) {
-        //   const newFixture = {
-        //     ...fixture,
-        //     round: [...fixture.round, response],
-        //   };
-        //   setFixture(newFixture);
-        // } else {
-        //   console.error("Fixture object is missing id property");
-        // }
-        setNewFixture(response);
+        if (fixture && fixture.id) {
+          const newFixture = {
+            ...fixture,
+            round: [...fixture.round, response],
+          };
+          setFixture(newFixture);
+        } else {
+          console.error("Fixture object is missing id property");
+        }
       }
-
-      const teamWinner = await getOneTeam(teamId);
-      setWinnerNames((prev) => ({ ...prev, [matchId]: teamWinner.name }));
-      setSelectedWinner(teamId);
-
       setDropdownOpen(null);
     } catch (error) {
       console.log(error);
@@ -100,13 +64,8 @@ const NewFixtureComponent: React.FC<FixtureProps> = ({ fixtureId }) => {
     Final: 1,
   };
 
-  console.log(newFixture);
-
   const stages = Object.keys(stagesConfig);
-  console.log(stages);
-  console.log(
-    fixture.round.map((round) => round.matches.map((match) => match.teams))
-  );
+
   return (
     <div className="flex flex-col items-center space-y-6 w-full">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-8 w-full border-2 border-red-600">
@@ -123,9 +82,6 @@ const NewFixtureComponent: React.FC<FixtureProps> = ({ fixtureId }) => {
               >
                 <p className="text-xs">{`Fecha: ${match.date}`}</p>
                 <p className="text-xs">{`Hora: ${match.time}`}</p>
-                <p className="text-xs">{`Ganador: ${
-                  winnerNames[match.id] || "Por decidir"
-                }`}</p>
 
                 <div className="flex flex-col space-y-1 mt-2">
                   {match?.teams?.map((team) => (
